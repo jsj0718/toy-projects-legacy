cf09833b1b35a0276a01d4f9822ed10df8e256b9
jsj0718 <jeongsejin718@gmail.com>
2021-05-29 21:50:47 +0900

    Final Commit (파일 업로드 및 다운로드 기능 구현, 보안 시큐어 코딩 등)

---
+++ File Upload/src/file/FileDTO.java
@@ -0,0 +1,37 @@
+package file;
+
+public class FileDTO {
+	private String fileName;
+	private String fileRealName;
+	private int downloadCount;
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileRealName() {
+		return fileRealName;
+	}
+	public void setFileRealName(String fileRealName) {
+		this.fileRealName = fileRealName;
+	}
+	public int getDownloadCount() {
+		return downloadCount;
+	}
+	public void setDownloadCount(int downloadCount) {
+		this.downloadCount = downloadCount;
+	}
+	
+	public FileDTO() {
+		
+	}
+	public FileDTO(String fileName, String fileRealName, int downloadCount) {
+		super();
+		this.fileName = fileName;
+		this.fileRealName = fileRealName;
+		this.downloadCount = downloadCount;
+	}
+
+}
